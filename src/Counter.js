daefaa58ff91e67383c1559db78c48c2787b3233
Aryan Sharma <106926421+ferrary7@users.noreply.github.com>
2023-01-10 12:35:48 +0530

    initial commit

---
+++ src/Counter.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { createStore } from 'redux';
+import reducer from './Reducers';
+import { incrementNumber, decrementNumber } from './Action';
+
+const store = createStore(reducer)
+
+
+export default function Counter() {
+
+  const [count, setCounter] = useState(0);
+
+  // eslint-disable-next-line no-unused-vars
+  const unsubscribe = store.subscribe(()=>{
+    setCounter(store.getState().count)
+  })  
+
+  return (
+    <div>
+      <h3>{count}</h3>
+      <button onClick={()=>store.dispatch(incrementNumber())}>Like</button>
+      <button onClick={()=>store.dispatch(decrementNumber())}>Unlike</button>
+    </div>
+  )
+}
